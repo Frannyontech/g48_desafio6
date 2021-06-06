e535d3ef02af57402b9d62e033f2aff251d54bb1
Frannyontech <fran.galazojeda@gmail.com>
2021-06-06 16:48:01 -0400

    Proyecto Ricomida

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+$(function (){
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+$( document ).ready(function(){
+    $( "#btn_mail" ).click(function(){
+        var section_alert = $('#myalert');
+
+        var nodo = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        </div>`
+        section_alert.append(nodo);
+    });
+});
+
